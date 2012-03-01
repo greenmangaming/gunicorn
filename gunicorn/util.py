2b410b600a459759af06473ec0066169dc200506
benoitc <bchesneau@gmail.com>
2012-03-01 14:53:33 +0100

    replace urlparse & StringIO call.

--- gunicorn/util.py
+++ gunicorn/util.py
@@ -22,6 +22,7 @@ import resource
 import socket
 import sys
 import textwrap
+import traceback
 import time
 
 from gunicorn import py3compat
@@ -124,7 +125,8 @@ def load_class(uri, default="sync", section="gunicorn.workers"):
                 return pkg_resources.load_entry_point("gunicorn",
                             section, uri)
             except ImportError:
-                raise RuntimeError("class uri invalid or not found")
+                traceback.print_exc()
+                raise RuntimeError("class uri invalid or not found.")
         klass = components.pop(-1)
         mod = __import__('.'.join(components))
         for comp in components[1:]:
